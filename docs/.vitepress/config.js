61b5ed9b7136372a75b0d7078c840acedd44241f
marsvillager <1968643693@qq.com>
2023-04-17 20:30:38 +0800

    new

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -8,8 +8,7 @@ export default defineConfig({
     // https://vitepress.dev/reference/default-theme-config
     nav: [
       { text: 'Home', link: '/' },
-      { text: 'Examples', link: '/markdown-examples' },
-      { text: 'Mindmaps', link: '/cvelistV5' }
+      { text: 'Examples', link: '/markdown-examples' }
     ],
 
     sidebar: [
