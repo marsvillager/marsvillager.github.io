f1a38650ce4c9faab095371620a52bd4e73318f0
marsvillager <1968643693@qq.com>
2024-03-05 16:51:11 +0800

    latex

--- docs/.vitepress/config.js
+++ docs/.vitepress/config.js
@@ -1,4 +1,93 @@
 import { defineConfig } from 'vitepress'
+import markdownItKatex from 'markdown-it-katex'
+
+const customElements = [
+  'math',
+  'maction',
+  'maligngroup',
+  'malignmark',
+  'menclose',
+  'merror',
+  'mfenced',
+  'mfrac',
+  'mi',
+  'mlongdiv',
+  'mmultiscripts',
+  'mn',
+  'mo',
+  'mover',
+  'mpadded',
+  'mphantom',
+  'mroot',
+  'mrow',
+  'ms',
+  'mscarries',
+  'mscarry',
+  'mscarries',
+  'msgroup',
+  'mstack',
+  'mlongdiv',
+  'msline',
+  'mstack',
+  'mspace',
+  'msqrt',
+  'msrow',
+  'mstack',
+  'mstack',
+  'mstyle',
+  'msub',
+  'msup',
+  'msubsup',
+  'mtable',
+  'mtd',
+  'mtext',
+  'mtr',
+  'munder',
+  'munderover',
+  'semantics',
+  'math',
+  'mi',
+  'mn',
+  'mo',
+  'ms',
+  'mspace',
+  'mtext',
+  'menclose',
+  'merror',
+  'mfenced',
+  'mfrac',
+  'mpadded',
+  'mphantom',
+  'mroot',
+  'mrow',
+  'msqrt',
+  'mstyle',
+  'mmultiscripts',
+  'mover',
+  'mprescripts',
+  'msub',
+  'msubsup',
+  'msup',
+  'munder',
+  'munderover',
+  'none',
+  'maligngroup',
+  'malignmark',
+  'mtable',
+  'mtd',
+  'mtr',
+  'mlongdiv',
+  'mscarries',
+  'mscarry',
+  'msgroup',
+  'msline',
+  'msrow',
+  'mstack',
+  'maction',
+  'semantics',
+  'annotation',
+  'annotation-xml'
+]
 
 // https://vitepress.dev/reference/site-config
 export default defineConfig({
@@ -19,10 +108,10 @@ export default defineConfig({
           // { text: 'Runtime API Examples', link: '/api-examples' },
           { text: '后量子密码', link: '/后量子密码' },
           { text: 'CTF_tools', link: '/CTF_tools' },
+          { text: 'CTF_Web_mysql_injection', link: '/CTF_Web_mysql_injection' },
           { text: 'CTF_Web_PNG图片转换器', link: '/CTF_Web_PNG图片转换器' },
           { text: 'CTF_Web_rce', link: '/CTF_Web_rce' },
           { text: 'CTF_Web_yeeclass', link: '/CTF_Web_yeeclass' },
-          { text: 'CTF_Web_yet_another_mysql_injection', link: '/CTF_Web_yet_another_mysql_injection' },
           { text: 'Floating Point', link: '/Floating Point' },
           { text: 'TLS', link: '/TLS' },
           { text: 'ZKP_Groth16', link: '/ZKP_Groth16' },
@@ -34,5 +123,18 @@ export default defineConfig({
     socialLinks: [
       { icon: 'github', link: 'https://github.com/marsvillager/marsvillager.github.io' }
     ]
+  },
+
+  markdown: {
+    config: (md) => {
+      md.use(markdownItKatex)
+    }
+  },
+  vue: {
+    template: {
+      compilerOptions: {
+        isCustomElement: (tag) => customElements.includes(tag)
+      }
+    }
   }
 })
--- docs/CTF_Web_mysql_injection.md
+++ docs/CTF_Web_mysql_injection.md
@@ -1,7 +1,7 @@
 ::: info
-2021-第五届空间智能安全大赛-Web-yet_another_mysql_injection
+2021-第五届空间智能安全大赛-Web-mysql_injection
 :::
-
+$test$
 #  1、思路
 
 （1）bp 抓包，POST 保存为 txt 文件
